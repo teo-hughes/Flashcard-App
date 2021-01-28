3e1e99e36cf4201b1b542319f4ad98024e49a92c
TEO OLIVER HUGHES <teo_hughes@icloud.com>
2021-01-28 15:19:59 +0000

    Initial Commit

---
+++ Flashcard App/Flashcard_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashcard_AppApp.swift
+//  Flashcard App
+//
+//  Created by Hughes, Teo (BJH) on 28/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Flashcard_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
